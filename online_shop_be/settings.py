1277208924eba4d5a3233cfba80e608508bf1a7b
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2022-03-12 04:36:56 +0100

    filtering and search in products view

--- online_shop_be/settings.py
+++ online_shop_be/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
             'name': 'store',
             'host': os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/storw',
             'username': 'Kcee',
-            "password": os.environ.get('MONGO_URI'),
+            "password": os.environ.get('MONGO_PASSWORD'),
             "authMechanism": "SCRAM-SHA-1"
         }
     }
