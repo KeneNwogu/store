8b492cfccd31f48673691eee35ce3a6d5c0e5c8a
[Kene Nwogu] <[nwogujoseph1111@gmail.com]>
2022-03-12 03:43:54 +0100

    filtering and search in products view

--- utilities/database.py
+++ utilities/database.py
@@ -1,6 +1,8 @@
+import os
+
 import pymongo
 
-connection_string = 'mongodb://127.0.0.1:27017/store'
+connection_string = os.environ.get('MONGO_URI')
 client = pymongo.MongoClient(connection_string)
 dbname = client['store']
 categories = dbname['categories']
