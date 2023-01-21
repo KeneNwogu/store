8ee6c6eaedca9b1aa19076146f572c7f1a65fc8b
[Kene Nwogu] <nwogujoseph1111@gmail.com>
2023-01-21 23:43:12 +0100

    added total number of pages to pagination links

--- utilities/pagination.py
+++ utilities/pagination.py
@@ -1,7 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class SmallResultsPagination(PageNumberPagination):
     page_size = 9
     page_query_param = 'page'
     max_page_size = 50
+
+    def get_paginated_response(self, data):
+        return Response({
+            'total_pages': self.page.paginator.num_pages,
+            'count': self.page.paginator.count,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data
+        })
